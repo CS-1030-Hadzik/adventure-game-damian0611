d262666b6a3e04dd3a6c9eb8fd0990052645361b
damian0611 <damianugalde@mail.weber.edu>
2025-02-04 21:18:48 -0800

    Created a multi-line comment and asked for players name.

--- game.py
+++ game.py
@@ -1,2 +1,16 @@
+'''Adventure Text Game
+Author: Damian Ugalde
+Version: 1.0
+Description: 
+This is a text-based game where the player navigates a world
+by making decisions.'''
+
+# Below is the welcome message and introduction.
 print("Welcome to the Adventure Game!")
 print("Your journey begins here...")
+
+# Ask for the Player's name.
+player_name = input('What is your name, oh great Adventurer? ')
+# Concatenate Strings to create a personalized message.
+print('Welcome, ' + player_name + '! Your journey begins now!')
+
